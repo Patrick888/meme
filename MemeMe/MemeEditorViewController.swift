57dc40a5e37c722b2b666956be97e777682fd1f9
patrick <leepc777@gmail.com>
2017-08-14 14:34:56 -0700

    for 8.14 review. the main difference is 1. remove all comment out codes 2. fix the keyboard (putting notification in viewWillAppear instead of viewDidLoad) 3. recover the save() in completion handler which I accident removed.

--- MemeMe/MemeEditorViewController.swift
+++ MemeMe/MemeEditorViewController.swift
@@ -24,13 +24,9 @@ class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegat
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         // disable cameraButton if the .camera source is not available.
-        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
         
-//        topTextField.textAlignment = .center
-//        botTextField.textAlignment = NSTextAlignment.center
         customizeTextField(topTextField)
         customizeTextField(botTextField)
-        subscribeToKeyboardNotifications()
 
         print("         viewDidLoad got called")
 
@@ -39,6 +35,7 @@ class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegat
     
     // MARK: customize the TexTField.
     func customizeTextField(_ textField:UITextField) {
+        
         let memeTextAttributes:[String:Any] = [
             NSStrokeColorAttributeName: UIColor.black,
             NSForegroundColorAttributeName: UIColor.white,
@@ -47,16 +44,17 @@ class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegat
         textField.delegate = self
         textField.defaultTextAttributes = memeTextAttributes
         textField.textAlignment = .center
-        subscribeToKeyboardNotifications()
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        
         super.viewWillAppear(true)
-//        customizeTextField(topTextField)
-//        customizeTextField(botTextField)
-//        subscribeToKeyboardNotifications()
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
         print("     viewWillAppear got called")
-        }
+        subscribeToKeyboardNotifications()
+        
+    }
     
     override func viewWillDisappear(_ animated: Bool) {
         
@@ -106,7 +104,6 @@ class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegat
     @IBAction func openImagePicker(_ sender: UIButton) {
         let imagePicker = UIImagePickerController()
         imagePicker.sourceType = sender.tag == 1 ? .photoLibrary : .camera
-//        imagePicker.sourceType = .photoLibrary
         print("     imagePicker.sourceType is \(imagePicker.sourceType) and the sender is : \(sender)")
         present(imagePicker, animated: true, completion: nil)
         imagePicker.delegate = self
@@ -119,61 +116,31 @@ class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegat
         let nextcontroller = UIActivityViewController(activityItems: [image], applicationActivities: nil)
         present(nextcontroller, animated: true, completion: nil)
         
-       /* storing the memephoto data and dismiss in completion handler, put closure expression into the property completionWithItemsHandler
-        open var completionWithItemsHandler: UIKit.UIActivityViewControllerCompletionWithItemsHandler? // set to nil after call
-        public typealias UIActivityViewControllerCompletionWithItemsHandler = (UIActivityType?, Bool, [Any]?, Error?) -> Swift.Void
-        */
+        /* storing the memephoto data and dismiss in completion handler, put closure expression into the property completionWithItemsHandler
+         open var completionWithItemsHandler: UIKit.UIActivityViewControllerCompletionWithItemsHandler? // set to nil after call
+         public typealias UIActivityViewControllerCompletionWithItemsHandler = (UIActivityType?, Bool, [Any]?, Error?) -> Swift.Void
+         */
         
         nextcontroller.completionWithItemsHandler = {
             (activity,completed,returnedItems,activityError) -> Void in
-//            print (" check out the value of 4 parameters in closure: ",activity,completed,returnedItems,activityError)
-            // Optional(__C.UIActivityType(_rawValue: com.apple.UIKit.activity.SaveToCameraRoll)) true nil nil
             if completed {
                 self.save()
-                print(" read out the memeArray in SaveMeme.swift => \(memeArray)")
-//                self.dismiss(animated: true, completion: nil)
+                print(" \(memeArray.count)  read out the memeArray in SaveMeme.swift => \(memeArray)")
+                //                self.dismiss(animated: true, completion: nil)
             }
         }
-        
-        /* simplifed version or create a normal function for completion handler.
-         ignore 4 closure parameters and always run save() //
-         nextcontroller.completionWithItemsHandler = { _ in self.save()}
-         
-         more concise, use placeholders
-        nextcontroller.completionWithItemsHandler = {
-            print (" check out the value of 4 parameters in closure: ",$0,$1,$2,$3)
-            if $1 {
-                self.save()
-                self.dismiss(animated: true, completion: nil)
-            }
-        }
-        use normal function
-        func completionFunction (activity:UIActivityType?, completed: Bool, items:[Any]?, error:Error?){
-            if completed {
-                save()
-                dismiss(animated: true, completion: nil)
-            }
-        }
-        nextcontroller.completionWithItemsHandler = completionFunction
 
-        this dismiss wont' work because it excute right after opening.
-        dismiss(animated: true, completion: nil)
-         */
-        
     }
     
     
-    // MARK:TWO buttons Camear and Album call Delegate method from UIImagePickerControllerDelegate protocol to pass the selected image to imageView.
+    // MARK: Delegate Method. TWO buttons Camear and Album call Delegate method from UIImagePickerControllerDelegate protocol to pass the selected image to imageView.
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-//        imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
         if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
             imageView.image = image
             print("  delegate method imagePickerController() got called and assigned the image to imageView.image")
         } else { print("read out NIL,failed to read out image from album") }
         picker.dismiss(animated: true, completion: nil)
     }
-    
-    
     
     
     /* Render views to an image :render view(the original image and overlayed text) into a UIIamge object and return that new UIImage.
@@ -185,39 +152,27 @@ class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegat
 
     func generateMemedImage() -> UIImage {
         
-        // hide tool bar before rendering
-//        UINavigationBar.ishide
-//        topToolBar.isHidden = true
         botToolBar.isHidden = true
-        
         UIGraphicsBeginImageContext(self.view.frame.size)
         view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
         let memedImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
         UIGraphicsEndImageContext()
         print("call rendering, what is view : ", type(of: view))
-        
-        // unhide tool bar after rendering
-//        topToolBar.isHidden = false
         botToolBar.isHidden = false
-        
         return memedImage
     }
     
     
     // MARK: delegate method textFieldShouldReturn (UITextFieldDelegate protocol) to hide keyboard after tapping rerturn key
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
         textField.resignFirstResponder()
         return false // both false and true work here.
     }
 
     // MARKS: save the new rendered image to struct for future use
     func save() {
-//        struct Meme {
-//            var topText: String
-//            var bottomText: String
-//            var originalImage: UIImage
-//            var memedImage: UIImage
-//        }
+        
         let memedImage = generateMemedImage()
         let meme = Meme(topText: topTextField.text!, bottomText: botTextField.text!, originalImage: imageView.image!, memedImage: memedImage)
         memeArray.append(meme)
